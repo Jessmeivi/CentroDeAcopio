9b84af8ee823e482f26c2fc3693f89f5119e1f2c
Jessie <jessie.gc2213@gmail.com>
2020-11-22 15:16:28 -0600

    basico_graficos

---
+++ elim_emp.php
@@ -0,0 +1,15 @@
+<?php
+    include('database.php');
+    $conexion=conectar();
+    if(!$conexion)
+    {
+        die("error de conexion");
+    }
+    echo "<hr style='border-color: red'>";
+    echo "<br>";
+    $eliminar=$_POST['enviar2'];
+    $sql ="DELETE from producto
+    where id_prod=$eliminar"; //Con esta consulta filtra todas las preguntas que contengan esa palabra clave
+    $result = $conexion->query($sql);
+    
+?>
